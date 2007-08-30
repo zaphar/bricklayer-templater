b2c898518669ee82f9fa98926cbace832fc711d6
jeremy <jeremy@marzhillstudios.com>
2007-08-29 22:33:24 -0500

    debugging flag for common::object handler
    darcs-hash:20070830033324-f4041-5df48026148d9e341b412e771bf81fd854b8e6fa.gz

--- lib/Bricklayer/Templater/Handler/common/object.pm
+++ lib/Bricklayer/Templater/Handler/common/object.pm
@@ -16,30 +16,36 @@ use base qw(Bricklayer::Templater::Handler);
 sub run {
 	my ($self, $object) = @_;
 	my $retrieve = $self->attributes()->{call};
-
+	my $debug = $self->attributes()->{debug};
+    carp("---Object is: ".ref($object)) if $debug;
+    carp("---requesting: $retrieve") if $debug;
     my $passthrough = $self->attributes()->{nest};
 	my $negate = $self->attributes()->{"not"};
 	if (ref($object) ne "") {
 		my $return;
 		$retrieve =~ s/\./->/g;
 		my $call = '$return = $object->'.$retrieve;
+        carp("---running: [$call]") if $debug;
 		eval $call;
-
+        croak($@) if $@;
+        carp("---the return was: $return") if defined $return and $debug;
 		my $arg;
 		if ($self->block) {
-
+            carp('******'.$self->block()) if $debug;
 			$arg = $return if $passthrough;
 			$arg = $object unless $passthrough;
-
 			if ($return || $negate) {
 				return if $negate && $return;
 			} else {
 				return;
 			}
 			if ($self->attributes->{embed}) {
+                carp("---the arg is: ".ref($arg)) if $debug;
 				return &$arg();
 			} else {
-				$self->parse_block($arg);
+                carp("---before parse_block the arg is: ".ref($arg)) if $debug;
+                $self->parse_block($arg);
+                carp("---after parse_block the arg is: ".ref($arg)) if $debug;
 			}
 			return;
 		}
